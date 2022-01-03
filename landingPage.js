f5fec4cd33f5fd8ef4125321ab98bd48e8b90253
TessyJr <christopher.nathanael1217@gmail.com>
2022-01-03 10:57:54 +0700

    Updated landingPage .html .css .js and added more assets

--- landingPage.js
+++ landingPage.js
@@ -1,7 +1,28 @@
-window.onscroll = function(){  
+let section = document.querySelectorAll('section');
+let navLinks = document.querySelectorAll('nav a');
+
+window.onscroll = () =>{
+    //changing navbar
     if(window.pageYOffset > 5){
         document.getElementById("navigationBar").style.backgroundColor = "rgba(51, 66, 87, 0.8)";
     } else {
         document.getElementById("navigationBar").style.backgroundColor = "rgba(51, 66, 87, 0)";
     }
-}
+
+    //scrollspy
+    section.forEach(sec =>{
+
+        let top = window.scrollY;
+        let offset = sec.offsetTop - 150;
+        let height = sec.offsetHeight;
+        let id = sec.getAttribute('id');
+
+        if(top >= offset && top < offset + height){
+            navLinks.forEach(links =>{
+                links.classList.remove('active');
+                document.querySelector('nav a[href*=' + id + ']').classList.add('active');
+
+            })
+        }
+    })
+};
